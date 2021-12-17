f1877fb0d1bdf32abc0896cd68d54b7b21b07db6
Sebastian Segui Sanchez <sebas@correo.com>
2021-12-17 17:00:33 +0100

    Base del proyecto

---
+++ SliderColores/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module es.ideas.slidercolores {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens es.ideas.slidercolores to javafx.fxml;
+    exports es.ideas.slidercolores;
+}
+*/
